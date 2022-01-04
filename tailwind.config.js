552ba52510256b3d1477db50b05733b0397347ca
Jovanka Samudra <51015175+samudrajovanka@users.noreply.github.com>
2022-01-04 16:27:12 +0700

    feat: navbar (#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
       sans: ['Open Sans', 'sans-serif'],
       fancy: ['Signika Negative', 'cursive'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#FFFFFF',
+      dark: '#0F0F0F',
+      'soft-dark': '#2F2F2F',
+      blue: '#5FFFD8',
+      orange: '#FF7F57',
+      pink: '#FF5880',
+      green: '#A1FF29',
+      yellow: '#FFDD29',
     },
     extend: {},
   },
