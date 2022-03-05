7dc17758b3923d562cd410cec51bc074e905c148
Jovanka <samudrajovanka20@gmail.com>
2022-03-05 10:36:50 +0700

    fix: host in config env

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 const path = require('path');
 
+const getHost = () => {
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV) {
+    switch (process.env.NEXT_PUBLIC_VERCEL_ENV) {
+      case 'production':
+        return 'https://jovanka-samudra.me';
+      default:
+        return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    }
+  } else {
+    return 'http://localhost:3000';
+  }
+};
+
 // eslint-disable-next-line arrow-body-style
 module.exports = () => {
   return {
     env: {
-      HOST: process.env.NEXT_PUBLIC_VERCEL_URL
-        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-        : 'http://localhost:3000',
+      HOST: getHost(),
       API_KEY: process.env.API_KEY,
     },
     sassOptions: {
